c8d7b4c4d538f3123bf7b7339e925e3b99fe0614
Shrey <shreyg0e1dev@gmail.com>
2026-02-08 03:14:07 +0530

    Day02: Import & Export in Details

---
+++ Day02/Today/calculator/index.js
@@ -0,0 +1,5 @@
+const sum = require("./sum");
+const sub = require("./sub");
+const mul = require("./mul");
+
+module.exports= {sum,sub,mul}
---
+++ Day02/Today/first.js
@@ -0,0 +1,16 @@
+// const sum = require("./current/sum");
+// const sub = require("./current/sub");
+// const mul = require("./current/mul");
+
+
+const {sum,sub,mul} = require("./calculator");
+// sum
+sum(2,4);
+
+// sub
+sub(3,4);
+
+// sub
+mul(6,8);
+
+console.log("Hello ji");
