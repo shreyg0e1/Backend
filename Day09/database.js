9f1268586cf12b2ca5433033d9db9ea1e9f17467
Shrey <shreyg0e1dev@gmail.com>
2026-02-21 13:52:15 +0530

    Learned some operations like inserting, finding documents in database

--- Day09/database.js
+++ Day09/database.js
@@ -3,7 +3,10 @@ const { MongoClient } = require('mongodb');
 // import { MongoClient } from 'mongodb'
 
 // Connection URL: url helps to connect with cluster
-const url = "mongodb+srv://Shreygoel:ShreyG0e1%4023@codingadda.9tsduo1.mongodb.net/?appName=CodingAdda";
+// username password cluster 
+// @ == hexadecimal : 0x40
+const url = "mongodb://Shreygoel:ShreyG0e1%4023@ac-sueqhml-shard-00-00.9tsduo1.mongodb.net:27017,ac-sueqhml-shard-00-01.9tsduo1.mongodb.net:27017,ac-sueqhml-shard-00-02.9tsduo1.mongodb.net:27017/?authSource=admin&replicaSet=atlas-d3rvx5-shard-0&tls=true";
+// const url = "mongodb+srv://Shreygoel:ShreyG0e1%4023@codingadda.9tsduo1.mongodb.net/?tls=true";
 const client = new MongoClient(url);
 
 // Database Name
@@ -17,6 +20,30 @@ async function main() {
   const collection = db.collection('user');
 
   // the following code examples can be pasted here...
+
+
+  // Find All Documents: Documents ko laana 
+  const findResult =  collection.find({});
+  // const ans = await findResult.toArray();
+    // for await (const doc of findResult)
+    // console.log(doc);
+  // console.log('Found documents =>',ans);
+
+
+
+
+//   // Insert a single Document
+//   const insertResult = await collection.insertOne({ name: "Saumya", age:23 });
+//  console.log('Inserted documents =>', insertResult);
+
+//   // Insert a many Document
+// const insertResult = await collection.insertMany([{ a: 1 }, { a: 2 }, { a: 3 }]);
+// console.log('Inserted documents =>', insertResult);
+
+
+// Find Documents with a Query Filter
+const filteredDocs = await collection.find({ a: 3 }).toArray();
+console.log('Found documents filtered by { a: 3 } =>', filteredDocs);
 
   return 'done.';
 }
